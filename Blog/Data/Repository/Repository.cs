3957e94818a05d0eae1b5581f0c4fb377660f5ab
TelmanYusifov <43857129+TelmanYusifov@users.noreply.github.com>
2021-01-03 15:06:59 +0400

    Smart pagination changed.

--- Blog/Data/Repository/Repository.cs
+++ Blog/Data/Repository/Repository.cs
@@ -45,15 +45,49 @@ namespace Blog.Data.Repository
                 query = query.Where(post => post.Category.ToLower().Equals(Category.ToLower()));
 
             int postsCount = query.Count();
-
+            int pageCount = (int)Math.Ceiling((double)postsCount / pageSize);
             return new IndexViewModel
             {
                 PageNumber = pageNumber,
-                PageCount = (int)Math.Ceiling((double)postsCount / pageSize),
+                PageCount = pageCount,
                 NextPage = postsCount > skipAmount + pageSize,
+                Pages = PageNumbers(pageNumber, pageCount),
                 Category = Category,
                 Posts = query.Skip(skipAmount).Take(pageSize).ToList()
             };
+        }
+
+        private IEnumerable<int> PageNumbers(int pageNumber, int pageCount)
+        {
+            int midPoint = pageNumber < 3 ? 3
+                :pageNumber > pageCount - 2 ? pageCount - 2
+                :pageNumber;
+
+            int lowerBound = midPoint - 2;
+            int upperBound = midPoint + 2;
+
+            if (lowerBound != 1)
+            {
+                yield return 1;
+                if (lowerBound - 1 > 1)
+                {
+                    yield return -1;
+                }
+            }
+
+            for (int i = midPoint - 2; i <= midPoint + 2; i++)
+            {
+                yield return i;
+            }
+
+            if (upperBound != pageCount)
+            {
+                if (pageCount - upperBound > 1)
+                {
+                    yield return -1;
+                }
+                yield return pageCount;
+            }
         }
 
         public Post GetPost(int id)
